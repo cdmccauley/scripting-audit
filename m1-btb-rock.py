821a98adc921ef648b5caac827e473ee88d0c515
Chris McCauley <cdmccauley@gmail.com>
2020-03-03 15:01:08 -0700

    reviewing project

--- m1-btb-rock.py
+++ m1-btb-rock.py
@@ -1,12 +1,12 @@
 # demonstrates: comments, print, escape character, string literal
 # print a header for the game
-# print("\n****Welcome to Rock, Paper, Scissors****\n")
+print("\n**** Welcome to Rock, Paper, Scissors ****\n")
 
 # demonstrates: comments, print, string literal
 # print a header for the game
-print("")
-print("****Welcome to Rock, Paper, Scissors, lizard, spock****")
-print("")
+# print("")
+# print("**** Welcome to Rock, Paper, Scissors, lizard, spock ****")
+# print("")
 
 # get player 1 input
 player1 = raw_input("Player 1, choose: rock, paper, scissors, lizard, or spock: ")
@@ -25,13 +25,41 @@ elif (player1 == "rock" and player2 == "paper"):
     print("Player 2 wins, paper covers rock.")
 elif (player1 == "rock" and player2 == "scissors"):
     print("Player 1 wins, rock smashes scissors.")
+elif (player1 == "rock" and player2 == "lizard"):
+    print("Player 1 wins, rock smashes lizard")
+elif (player1 == "rock" and player2 == "spock"):
+    print("Player 2 wins, Spock vaporizes rock.")
 elif (player1 == "paper" and player2 == "rock"):
     print("Player 1 wins, paper covers rock.")
 elif (player1 == "paper" and player2 == "scissors"):
     print("Player 2 wins, scissors cut paper.")
+elif (player1 == "paper" and player2 == "lizard"):
+    print("Player 2 wins, lizard eats paper.")
+elif (player1 == "paper" and player2 == "spock"):
+    print("Player 1 wins, paper disproves Spock")
 elif (player1 == "scissors" and player2 == "rock"):
     print("Player 2 wins, rock smashes scissors.")
 elif (player1 == "scissors" and player2 == "paper"):
-    print("player 1 wins, scissors cut paper.")
+    print("Player 1 wins, scissors cut paper.")
+elif (player1 == "scissors" and player2 == "lizard"):
+    print("Player 1 wins, scissors decapitate lizard.")
+elif (player1 == "scissors" and player2 == "spock"):
+    print("Player 2 wins, Spock smashes scissors.")
+elif (player1 == "lizard" and player2 == "rock"):
+    print("Player 2 wins, rock smashes lizard.")
+elif (player1 == "lizard" and player2 == "paper"):
+    print("Player 1 wins, lizard eats paper.")
+elif (player1 == "lizard" and player2 == "scissors"):
+    print("Player 2 wins, scissors decapitate lizard.")
+elif (player1 == "lizard" and player2 == "spock"):
+    print("Player 1 wins, lizard poisons Spock.")
+elif (player1 == "spock" and player2 == "rock"):
+    print("Player 1 wins, Spock vaporizes rock.")
+elif (player1 == "spock" and player2 == "paper"):
+    print("Player 2 wins, paper disproves Spock.")
+elif (player1 == "spock" and player2 == "scissors"):
+    print("Player 1 wins, Spock smashes scissors.")
+elif (player1 == "spock" and player2 == "lizard"):
+    print("Player 2 wins, lizard poisons Spock.")
 else:
     print("Error: Somebody did not select rock, paper, or scissors.")
