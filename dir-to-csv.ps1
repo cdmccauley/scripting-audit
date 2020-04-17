f5ce0c8db2f280b294a226545af652ff5e7ad3fc
cdmccauley <cdmccauley@gmail.com>
2020-04-17 14:18:22 -0600

    complete

---
+++ dir-to-csv.ps1
@@ -0,0 +1,5 @@
+﻿$SearchPath = Read-Host "Please enter path to search"
+Get-ChildItem $SearchPath -Recurse -ErrorAction SilentlyContinue | 
+Select Name, FullName | 
+ConvertTo-Csv |
+Tee-Object -FilePath "C:\search-path.csv"
