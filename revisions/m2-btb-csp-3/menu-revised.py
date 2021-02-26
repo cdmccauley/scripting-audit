ea961e2b591dfba32fa4ac7c16a469f96572d301
Chris McCauley <cdmccauley@gmail.com>
2021-02-26 13:48:42 -0700

    simplifyng menu

--- revisions/m2-btb-csp-3/menu-revised.py
+++ revisions/m2-btb-csp-3/menu-revised.py
@@ -1,71 +1,37 @@
-# imports
-import os, time
+# reversing a string, demonstrating functions
 
-# get string from user
-def getString():
-    userString = raw_input("Please enter a string: ")
-    return userString
 
-# print a menu of options
+# get user input
+
+# reverse string
+
+# continue?
+
+def getInputString():
+    userInput = raw_input("enter string")
+    return userInput
+
 def printMenu():
-    time.sleep(2) # display last entry
-    os.system('cls') # clear ui
     print "Menu:"
-    print "1: Print the string in reverse..."
-    print "2: Print the length of the string..."
-    print "3: Print the string in uppercase..."
-    print "4: Print the string in lowercase..."
-    print "5: Enter new string..."
-    print "6: Exit..."
-    print ""
+    print "1. Enter new string"
+    print "2. Check if string is palindrome"
+    print "3. Quit"
 
-# get menu item from user
-#NOTE: working here
-def getMenuItem():
-    menuItem = 0
-    userInput = raw_input("Enter an item number from the menu:")
-    try:
-        menuItem = int(userInput)
-    except:
-        print "Error: Input must be a number 1-6."
-    if menuItem > 0 and menuItem < 7:
-        return menuItem
-    else:
-        getMenuItem()
+def menuPrompt():
+    menuItem = raw_input("Please enter menu item (1-3): ")
+    return menuItem
 
-# demonstrates: function parameter
-def revString(user_string):
-    rev_string=''
-    x=len(user_string) #gets the length of a string
-    for i in range(0, x):
-        rev_string=user_string[i]+rev_string
-    print "Your string in reverse:",rev_string
-    if (user_string.lower())== (rev_string.lower()):
-        print "Your string is a palindrome!"
-    print ""
+def reverseString(userString):
+    return ""
 
-# demonstrates: function arguments
-user_string = getString()
-again=True
-while (again):
-    printMenu()
-    choice = getMenuItem()
-	#create output
-    if choice==1:
-        revString(user_string)
-    elif choice==2: 
-        print "The length of your string is: ",str(len(user_string))
-    elif choice==3: 
-        print "Your string in all-Caps: ",user_string.upper()
-    elif choice==4: 
-        print "Your string in lowercase: ",user_string.lower() 
-    elif choice==5:
-        user_string = getString()
-    else:  #choice==6: 
-        again=False
-        print ""
-print ""
-pause=raw_input("Press Enter to close the window...")
+def exitScript():
+    return False
 
-
+def menuSelection(item):
+    if item == "1":
+        getInputString()
+    elif item == "2":
+        reverseString(item)
+    elif item == "3":
+        exitScript()
 
