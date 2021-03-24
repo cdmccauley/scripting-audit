c994bb47e66134c9ee232b186b77ad5f5cb645a3
Chris McCauley <cdmccauley@gmail.com>
2021-03-24 17:48:11 -0600

    revision needs review

--- revisions/m2-btb-csp-3/menu-revised.py
+++ revisions/m2-btb-csp-3/menu-revised.py
@@ -1,46 +1,48 @@
-users = { 1234: 'Robbie Sweeten', 5678: 'Stephen Christensen' }
+# variable declarations
+users = { 1234: 'AJ Hepler', 5678: 'Robbie Sweeten', 1357: 'Stephen Christensen' }
 output = ''
 
+# function declarations
 def addOrUpdateUser(id, name):
-    if id not in users.keys():
-        response = 'User added'
+    if id not in users.keys(): # check if user id does not exist
+        response = 'User ' + name + ' added' # user id does not exist, add user
     else:
-        response = 'User updated'
+        response = 'User ' + users[id] + ' updated to ' + name # user id exists, update user
     users[id] = name
     return response
 
 def removeUser(id):
-    if id in users.keys():
-        users.pop(id)
-        response = 'User removed'
+    if id in users.keys(): # check if user id exists
+        response = 'User ' + users[id] + ' removed'
+        users.pop(id) # user id exists, remove user using id
     else:
-        response = 'User not found'
+        response = 'User ID' + id + ' not found'
     return response
 
 def printUsers():
-    for key in users.keys():
-        print str(key) + ': ' + users[key]
+    for key in users.keys(): # loop through keys and store user id in key
+        print str(key) + ': ' + users[key] # print user key and user name by accessing key
 
 def searchUserNames(name):
-    response = 'User not found'
-    for value in users.values():
-        if name in value:
-            response = 'User found'
+    response = 'User ' + name + ' not found'
+    for value in users.values(): # loop through values and store user name in value
+        if name in value: # if the name provided matches the user name in value
+            response = 'User ' + name + ' found' 
     return response
 
+# begin script
+# call functions with arguments and store returned values
 output = addOrUpdateUser(2468, 'Carey Anson')
 print output
 output = searchUserNames('Carey Anson')
 print output
 
-output = addOrUpdateUser(5678, 'The Steve')
-print output
-output = searchUserNames('Stephen Christensen')
-print output
+# call functions with arguments and use returned values
+print addOrUpdateUser(1357, 'The Steve')
+print searchUserNames('Stephen Christensen')
 
-output = removeUser(1234)
-print output
-output = searchUserNames('Robbie Sweeten')
-print output
+print removeUser(1234)
+print searchUserNames('AJ Hepler')
 
+# call function that does not require arguments or return a value
 printUsers()
