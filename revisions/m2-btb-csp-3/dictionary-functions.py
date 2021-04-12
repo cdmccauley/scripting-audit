dd298c30453b0c529b829d332b4611c6b1b8aedd
Chris McCauley <cdmccauley@gmail.com>
2021-04-12 11:03:00 -0600

    generalizing functions

--- revisions/m2-btb-csp-3/dictionary-functions.py
+++ revisions/m2-btb-csp-3/dictionary-functions.py
@@ -3,20 +3,20 @@ users = { 1234: 'AJ Hepler', 5678: 'Robbie Sweeten', 1357: 'Stephen Christensen'
 output = ''
 
 # function declarations
-def addOrUpdateUser(id, name):
-    if users.has_key(id): # check if user id exists
-        response = 'User ' + users[id] + ' updated to ' + name # user id exists, update user
+def addOrUpdatePair(dictionary, key, value):
+    if dictionary.has_key(key): # check if key exists
+        response = 'Value ' + dictionary[key] + ' updated to ' + value # key exists, update value
     else:
-        response = 'User ' + name + ' added' # user id does not exist, add user
-    users[id] = name
+        response = 'Value ' + value + ' added' # key does not exist, add key/value
+    dictionary[key] = value
     return response
 
-def removeUser(id):
-    if users.has_key(id): # check if user id exists
-        response = 'User ' + users[id] + ' removed'
-        users.pop(id) # user id exists, remove user using id
+def removePairById(dictionary, key):
+    if dictionary.has_key(key): # check if user key exists
+        response = 'User ' + dictionary[key] + ' removed'
+        dictionary.pop(key) # user key exists, remove user using key
     else:
-        response = 'User ID' + id + ' not found'
+        response = 'User ID' + key + ' not found'
     return response
 
 def printUsers():
@@ -32,16 +32,16 @@ def searchUserNames(name):
 
 # begin script
 # call functions with arguments and store returned values
-output = addOrUpdateUser(2468, 'Carey Anson')
+output = addOrUpdatePair(users, 2468, 'Carey Anson')
 print output
 output = searchUserNames('Carey Anson')
 print output
 
 # call functions with arguments and use returned values
-print addOrUpdateUser(1357, 'The Steve')
+print addOrUpdatePair(users, 1357, 'The Steve')
 print searchUserNames('Stephen Christensen')
 
-print removeUser(1234)
+print removePairById(users, 1234)
 print searchUserNames('AJ Hepler')
 
 # call function that does not require arguments or return a value
