8f8cffd1a8531a8053b8370d74ca6303b77eddf5
Chris McCauley <cdmccauley@gmail.com>
2021-04-16 14:59:34 -0600

    more changes for generalization

--- revisions/m2-btb-csp-3/dictionary-functions.py
+++ revisions/m2-btb-csp-3/dictionary-functions.py
@@ -1,48 +1,62 @@
 # variable declarations
-users = { 1234: 'AJ Hepler', 5678: 'Robbie Sweeten', 1357: 'Stephen Christensen' }
+numbers = { 1: 'one', 2: 'two', 3: 'tree' }
 output = ''
 
 # function declarations
-def addOrUpdatePair(dictionary, key, value):
+def addPairOrUpdateValue(dictionary, key, value):
     if dictionary.has_key(key): # check if key exists
-        response = 'Value ' + dictionary[key] + ' updated to ' + value # key exists, update value
+        response = 'Value %s updated to %s.' %(dictionary[key], value) # key exists, update value
     else:
-        response = 'Value ' + value + ' added' # key does not exist, add key/value
+        response = 'Pair "%s: %s" added.' %(key, value) # key does not exist, add key/value
     dictionary[key] = value
     return response
 
-def removePairById(dictionary, key):
-    if dictionary.has_key(key): # check if user key exists
-        response = 'User ' + dictionary[key] + ' removed'
-        dictionary.pop(key) # user key exists, remove user using key
+def removePairByKey(dictionary, key):
+    if dictionary.has_key(key): # check if key exists
+        response = 'Pair "%s: %s" removed.' %(key, dictionary[key])
+        dictionary.pop(key) # key exists, remove key/value
     else:
-        response = 'User ID' + key + ' not found'
+        response = 'Key %s not found.' %(key)
     return response
 
-def printUsers():
-    for key in users.keys(): # loop through keys and store user id in key
-        print str(key) + ': ' + users[key] # print user key and user name by accessing key
-
-def searchUserNames(name):
-    response = 'User ' + name + ' not found'
-    for value in users.values(): # loop through values and store user name in value
-        if name in value: # if the name provided matches the user name in value
-            response = 'User ' + name + ' found' 
+def removePairByValue(dictionary, value):
+    response = 'Value %s not found.' %(value)
+    for key in dictionary.copy().keys(): # iterate over copy so no error is caused when removing from original
+        if dictionary[key] == value:
+            dictionary.pop(key) # value was found, remove key/value
+            response = 'All instances of %s removed.' %(value)
     return response
+
+def keyOrValue(dictionary, item):
+    if item in dictionary.keys() and item in dictionary.values():
+        response = '%s is a key and a value.' %(item)
+    elif item in dictionary.keys():
+        response = '%s is a key.' %(item)
+    elif item in dictionary.values():
+        response = '%s is a value.' %(item)
+    else:
+        response = '%s is not a key or a value.' %(item)
+    return response
+
+def printPairs(dictionary):
+    for key in dictionary.keys():
+        print '%s: %s' %(key, dictionary[key])
 
 # begin script
 # call functions with arguments and store returned values
-output = addOrUpdatePair(users, 2468, 'Carey Anson')
-print output
-output = searchUserNames('Carey Anson')
-print output
 
 # call functions with arguments and use returned values
-print addOrUpdatePair(users, 1357, 'The Steve')
-print searchUserNames('Stephen Christensen')
-
-print removePairById(users, 1234)
-print searchUserNames('AJ Hepler')
 
 # call function that does not require arguments or return a value
-printUsers()
+
+print addPairOrUpdateValue(numbers, 3, 'three')
+printPairs(numbers)
+
+print addPairOrUpdateValue(numbers, 4, 'four')
+printPairs(numbers)
+
+print removePairByKey(numbers, 1)
+printPairs(numbers)
+
+print removePairByValue(numbers, 'two')
+printPairs(numbers)
