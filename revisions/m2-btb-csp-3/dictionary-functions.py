bbdcd115bee077f91e7680b5d58e60d2d0b1b5ad
Chris McCauley <cdmccauley@gmail.com>
2021-04-30 14:57:18 -0600

    more updates

--- revisions/m2-btb-csp-3/dictionary-functions.py
+++ revisions/m2-btb-csp-3/dictionary-functions.py
@@ -5,53 +5,51 @@ output = ''
 # function declarations
 def addPairOrUpdateValue(dictionary, key, value):
     if dictionary.has_key(key): # check if key exists
-        response = 'Value %s updated to %s.' %(dictionary[key], value) # key exists, update value
+        response = 'Value {} updated to {}.'.format(dictionary[key], value) # key exists, function will update value
     else:
-        response = 'Pair "%s: %s" added.' %(key, value) # key does not exist, add key/value
-    dictionary[key] = value
+        response = 'Pair "{}: {}" added.'.format(key, value) # key does not exist, function will add key/value
+    dictionary[key] = value # update or add
     return response
 
 def removePairByKey(dictionary, key):
     if dictionary.has_key(key): # check if key exists
-        response = 'Pair "%s: %s" removed.' %(key, dictionary[key])
+        response = 'Pair "{}: {}" removed.'.format(key, dictionary[key])
         dictionary.pop(key) # key exists, remove key/value
     else:
-        response = 'Key %s not found.' %(key)
+        response = 'Key {} not found.'.format(key)
     return response
 
 def removePairByValue(dictionary, value):
-    response = 'Value %s not found.' %(value)
+    response = 'Value {} not found.'.format(value)
     for key in dictionary.copy().keys(): # iterate over copy so no error is caused when removing from original
         if dictionary[key] == value:
             dictionary.pop(key) # value was found, remove key/value
-            response = 'All instances of %s removed.' %(value)
+            response = 'All instances of {} removed.'.format(value)
     return response
 
 def keyOrValue(dictionary, item):
     if item in dictionary.keys() and item in dictionary.values():
-        response = '%s is a key and a value.' %(item)
+        response = '{} is a key and a value.'.format(item)
     elif item in dictionary.keys():
-        response = '%s is a key.' %(item)
+        response = '{} is a key.'.format(item)
     elif item in dictionary.values():
-        response = '%s is a value.' %(item)
+        response = '{} is a value.'.format(item)
     else:
-        response = '%s is not a key or a value.' %(item)
+        response = '{} is not a key or a value.'.format(item)
     return response
 
 def printPairs(dictionary):
     for key in dictionary.keys():
-        print '%s: %s' %(key, dictionary[key])
+        print '{}: {}'.format(key, dictionary[key])
 
 # begin script
-# call functions with arguments and store returned values
 
-# call functions with arguments and use returned values
-
-# call function that does not require arguments or return a value
-
-print addPairOrUpdateValue(numbers, 3, 'three')
+# call function with arguments and store return
+output = addPairOrUpdateValue(numbers, 3, 'three')
+print output
 printPairs(numbers)
 
+# call functions with arguments and use return
 print addPairOrUpdateValue(numbers, 4, 'four')
 printPairs(numbers)
 
