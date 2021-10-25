5308b5d6a95820ee485826afbed84b87de0af0dd
Chris McCauley <cdmccauley@gmail.com>
2021-10-25 15:42:55 -0600

    new lab code published

--- revisions/m5-btb-usp-3/code-snippets/source.ps1
+++ revisions/m5-btb-usp-3/code-snippets/source.ps1
@@ -1,6 +1,21 @@
-{ Write-Output "writing from a script block" }
+# review inserts
+{ Write-Output "Writing output from a script block" }
 
-$scriptBlock = { Write-Output "writing from a script block" }
+$scriptBlock = { Write-Output "Writing output from a script block" }
 $scriptBlock
 
-Invoke-Command $scriptBlock
+Invoke-Command -ScriptBlock $scriptBlock
+
+# guided practice inserts
+$session = New-PSSession -ComputerName PLABDC01
+
+$session
+
+Invoke-Command -Session $session -ScriptBlock { Write-Output "Writing output from a script block" }
+
+$getIP = { Get-NetIPAddress | Select-Object InterfaceAlias, IPAddress }
+
+Invoke-Command -ScriptBlock $getIP
+
+# not included in assignment
+Invoke-Command -Session $session -ScriptBlock $getIP
