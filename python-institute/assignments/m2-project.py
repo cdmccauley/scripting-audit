65bda7b044787eea292864efefb14e9da1d09b6b
Chris McCauley <cdmccauley@gmail.com>
2022-02-15 17:38:04 -0700

    writing assignment in an effort to see a big picture

--- python-institute/assignments/m2-project.py
+++ python-institute/assignments/m2-project.py
@@ -4,22 +4,38 @@
 # print(bin(192))
 # print(hex(65152))
 
-# print(bin(0xFE80))
+# we learned about binary representation in python
+# one place we work with binary number is in networking
+# ipv4 addresses are commonly displayed as integer representations of binary numbers
+# you may have already learned about the concept of CIDR notation
+# manually converting a CIDR notated IP address is a great exercise but we can save that for our certification study
+# let's take a break from manually converting and make Python do it for us
 
-first = 192
-second = 168
-third = 1
-fourth = 2
+# get each octet of an ipv4 address
+int_first = 192
+int_second = 168
+int_third = 1
+int_fourth = 2
 
-# first = bin(first)[2:]
-# second = bin(second)[2:]
-# third = bin(third)[2:]
-# fourth = bin(fourth)[2:]
+# convert each octet to binary
+bin_first = bin(int_first)
+bin_second = bin(int_second)
+bin_third = bin(int_third)
+bin_fourth = bin(int_fourth)
 
+# test the result, print will convert each octet to a string representation
+print(bin_first, bin_second, bin_third, bin_fourth, sep=".") # 0b11000000.0b10101000.0b1.0b10
+# the result is correct but does not look as expected
+
+# since the binary is correct but not immediately readable, we can instead turn it into a string
 octetFormat = "08b"
-first = format(first, octetFormat)
-second = format(second, octetFormat)
-third = format(third, octetFormat)
-fourth = format(fourth, octetFormat)
+str_first = format(int_first, octetFormat)
+str_second = format(int_second, octetFormat)
+str_third = format(int_third, octetFormat)
+str_fourth = format(int_fourth, octetFormat)
 
-print(first, second, third, fourth, sep=".")
+print(str_first, str_second, str_third, str_fourth, sep=".")
+
+str_binary_ipv4 = str_first + "." + str_second + "." + str_third + "." + str_fourth
+
+print(str_binary_ipv4)
