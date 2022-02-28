8414237c0b0131ca6876c23656a54dff3fb42190
Chris McCauley <cdmccauley@gmail.com>
2022-02-28 14:49:31 -0700

    drafting practice ideas

--- python-institute/assignments/m2-project.py
+++ python-institute/assignments/m2-project.py
@@ -5,6 +5,7 @@
 # manually converting a CIDR notated IP address is a great exercise but we can save that for our certification study
 # let's take a break from manually converting and make Python do it for us
 
+# NOTE: this could be done via input and might be a good conversion candidate for the practice
 # set each octet of an ipv4 address
 int_first = 192
 int_second = 168
@@ -12,7 +13,7 @@ int_third = 1
 int_fourth = 2
 
 # set cidr notation
-int_cidr = 25
+int_cidr = 24
 
 # convert each octet to binary
 # bin_first = bin(int_first)
@@ -53,6 +54,7 @@ print(str_binary_ipv4) # 11000000.10101000.00000001.00000010
 
 # find how many bits are "on" for the network portion of the address
 int_qty_network = int(int_cidr)
+
 # find how many bits are "off" for the host portion of the address
 int_qty_host = int(32 - int_qty_network)
 
@@ -82,7 +84,31 @@ str_mix = str_mix_on + str_mix_off
 int_extra_network = int((int_mix - 1) * (int_mix - 1))
 
 # multiply our results by the proper string representations to create a full mask
-str_full_cidr = (str_full_on * (int_full_network - int_extra_network)) + str_mix + (str_full_off * int_full_host)
+# start by assigning the start of the mask
+str_full_cidr = str_full_on * (int_full_network - int_extra_network)
+# use compound assignment to add the connecting octet
+str_full_cidr += str_mix
+# use compound assignment to add the end of the mask
+str_full_cidr += str_full_off * int_full_host
 
 # check the results
 print(str_full_cidr)
+
+# NOTE: this portion is repeat code and should be part of the practice
+# get octets for a comparison via input
+int_first_compare = int(input("Enter the first octet: "))
+int_second_compare = int(input("Enter the second octet: "))
+int_third_compare = int(input("Enter the third octet: "))
+int_fourth_compare = int(input("Enter the fourth octet: "))
+
+# convert input to octets
+str_first_compare = format(int_first_compare, octetFormat)
+str_second_compare = format(int_second_compare, octetFormat)
+str_third_compare = format(int_third_compare, octetFormat)
+str_fourth_compare = format(int_fourth_compare, octetFormat)
+
+# concatenate the octets into a single string
+str_binary_compare = str_first_compare + str_separator + str_second_compare + str_separator + str_third_compare + str_separator + str_fourth_compare
+
+# print for comparison
+print(str_binary_compare)
